accc140f0c26138703f5ae43955eb343eb359e4d
Mateus Ravagni <mateusravagni@gmail.com>
2021-03-03 02:15:35 +0000

    Atualizada

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Ola, tudo bem? Eu sou Mateus Franklin Ravagni e moro em Barretos-SP.");
+    System.out.println("Ola tudo bem? Eu sou Mateus Franklin Ravagni e moro em Barretos-SP.");
   }
 }
